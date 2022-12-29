d9282b35bc152122eb2bb6a2f6a6e87af2ccd4d2
Đặng Ngọc Giang <56185586+giangdang362@users.noreply.github.com>
2022-12-29 18:02:27 +0700

    update UI responsive mobile

--- index.js
+++ index.js
@@ -1,4 +1,19 @@
-const menu = document.getElementsByClassName("mobile-menu");
-function handleOpenPopup() {
-    menu.style.display="block";
+
+function handleOpenPopup(){
+    const menuBtn = document.getElementById('moblieMenu');
+    const bgBlur = document.getElementById('bg-blur');
+    const hambergerBtn = document.getElementById('hamberger-btn');
+    bgBlur.style.display = "block";
+    hambergerBtn.style.visibility = 'hidden';
+    menuBtn.style.left = "0";
+    menuBtn.style.transitionDuration = '0.5s';
+}
+function handleClickOutside() {
+    const hambergerBtn = document.getElementById('hamberger-btn');
+    const menuBtn = document.getElementById('moblieMenu');
+    const bgBlur = document.getElementById('bg-blur');
+    hambergerBtn.style.visibility = 'visible';
+    menuBtn.style.left = "-1000px";
+    bgBlur.style.display = "none";
+    menuBtn.style.transitionDuration = '0.8s';
 }
